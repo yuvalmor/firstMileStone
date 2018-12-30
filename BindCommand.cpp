593a823012759d2506dafa25371d46e96342a3de
yuvalmor <yuvalmor94@gmail.com>
2018-12-30 17:20:40 +0200

    adding comments to BindCommand.cpp

--- BindCommand.cpp
+++ BindCommand.cpp
@@ -8,10 +8,9 @@
  * we need to get as input a direct path to the relevant place in the simulator,
  * or Alternative to get other variable that holds the path.
  */
-
 void BindCommand::doCommand() {
     TableManager* tableManager = TableManager::getInstance();
-    // Put address pattern in
+    // Put address pattern in the regex
     regex reg ("[\"/][a-z/-]+[\"]");
     smatch match;
     regex_search(bindTo,match,reg);
@@ -19,19 +18,20 @@ void BindCommand::doCommand() {
     if(!match.empty()){
         // Cut the quotation marks from the path
         string path = bindTo.substr(1, bindTo.size()-2);
+        // Add the variable name and its path to the relevant table
         tableManager->bindStringToPath(varName,path);
     } else {
         // Checks if the variable has an address
         if(tableManager->isVarBindToAddress(bindTo)){
-            // Get the address to bind from the table manager
+            // Get the variable address from the table manager
             string address = tableManager->getVarAddress(bindTo);
+            // Bind the address for the new variable
             tableManager->bindStringToPath(varName,address);
             // Update the initial value of the variable in the relevant table
             double value = tableManager->getVarValue(bindTo);
             tableManager->updateLocalVar(varName,value);
-
         } else{
-            // Cant bind variable to other variable that dose not have address
+            // Cant bind variable to other variable that dose not have address!
             throw exception();
         }
     }
