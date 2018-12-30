2a5a4748598d7be32a4d4a39707b258b2235352a
yuvalmor <yuvalmor94@gmail.com>
2018-12-30 17:34:26 +0200

    change the ifndef name to: SIMULATOR9492YN_MASTER

--- BooleanExpression.h
+++ BooleanExpression.h
@@ -1,21 +1,30 @@
-#ifndef NEWSIMULATOR_BOOLEANEXPRESSION_H
-#define NEWSIMULATOR_BOOLEANEXPRESSION_H
+#ifndef  SIMULATOR9492YN_MASTER_BOOLEANEXPRESSION_H
+#define  SIMULATOR9492YN_MASTER_BOOLEANEXPRESSION_H
 
 #include "Expression.h"
-#include <vector>
 #include <string>
 
+/**
+ * BooleanExpression contains two operands (represent as expression),
+ * separated by one operator.
+ * It produce true(!0) or false(0) when evaluate.
+ */
 class BooleanExpression : public Expression{
+    // Left operand
     Expression* leftExpression;
+    // Right operand
     Expression* rightExpression;
+    // The operator between the operands
     string condition;
 public:
+    // Constructor
     BooleanExpression(Expression* left, string condition, Expression* right){
         this-> leftExpression = left;
         this->rightExpression = right;
         this->condition = condition;
     }
+    // Calculate the boolean expression
     double calculate() override;
 };
 
-#endif //NEWSIMULATOR_BOOLEANEXPRESSION_H
+#endif
